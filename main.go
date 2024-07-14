fba40219d9d2d5cfc29ad06740f2f3015171599e
NikarashiHatsu <yourlovelydev@gmail.com>
2024-07-15 01:10:12 +0700

    chore (app): main functionality

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	
+	app := gin.Default()
+
+	route := app
+
+	route.GET("/", func(ctx *gin.Context) {
+		isValidated := false
+
+		if !isValidated {
+			ctx.AbortWithStatusJSON(400, gin.H{
+				"message": "bad request, some field are not validated",
+			})
+
+			return
+		}
+
+		ctx.JSON(200, gin.H{
+			"hello": "world",
+		})
+	})
+
+	app.Run()
 }
