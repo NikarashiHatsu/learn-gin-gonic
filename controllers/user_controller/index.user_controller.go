59b2b4ea53cd9ea5cefd3b2c2738a0d374912b6a
NikarashiHatsu <yourlovelydev@gmail.com>
2024-07-15 03:43:45 +0700

    chore (user): fetch all user

--- controllers/user_controller/index.user_controller.go
+++ controllers/user_controller/index.user_controller.go
@@ -1,9 +1,27 @@
 package user_controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-gonic-gorm/database"
+	"gin-gonic-gorm/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetAllUser(ctx *gin.Context) {
+	users := new([]models.User)
+
+	err := database.DB.Find(&users).Error
+
+	if err != nil {
+		ctx.AbortWithStatusJSON(500, gin.H{
+			"message": "Internal server error.",
+		})
+
+		return
+	}
+
 	ctx.JSON(200, gin.H{
-		"hello": "user",
+		"message": "Users fetched.",
+		"data":    users,
 	})
 }
